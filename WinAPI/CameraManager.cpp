1d29d881c6130b166a1b4cab2229e5b7a63d0a85
jae <pkj8595@gmail.com>
2022-03-02 11:33:11 +0900

    pkj : camera

--- WinAPI/CameraManager.cpp
+++ WinAPI/CameraManager.cpp
@@ -1,2 +1,43 @@
 #include "Stdafx.h"
 #include "CameraManager.h"
+
+HRESULT CameraManager::init()
+{
+
+	return S_OK;
+}
+
+HRESULT CameraManager::init(int displayAreaRight, int displayAreaBottom)
+{
+	_displayAreaRight = displayAreaRight;
+	_displayAreaBottom = displayAreaBottom;
+	_clientRect = &RectMake(0,0,0,0);
+	GetClientRect(_hWnd, _clientRect);
+
+	return S_OK;
+}
+
+void CameraManager::setPlayerPosition(float* x, float* y)
+{
+	_playerX = x;
+	_playerY = y;
+}
+
+void CameraManager::release()
+{
+}
+
+void CameraManager::startMappingMode(HDC hdc)
+{
+	SetMapMode(hdc, MM_ISOTROPIC);
+	SetWindowExtEx(hdc, _displayAreaRight, _displayAreaBottom, NULL);
+	SetViewportExtEx(hdc, _clientRect->right, _clientRect->bottom, NULL);
+
+}
+
+void CameraManager::endMappingMode(HDC hdc)
+{
+	SetMapMode(hdc, MM_ISOTROPIC);
+	SetWindowExtEx(hdc, _clientRect->right, _clientRect->bottom, NULL);
+	SetViewportExtEx(hdc, _clientRect->right, _clientRect->bottom, NULL);
+}
--- WinAPI/CameraManager.h
+++ WinAPI/CameraManager.h
@@ -3,10 +3,24 @@
 
 class CameraManager:public SingletonBase<CameraManager>
 {
-public:
 
 private:
+	float* _playerX;
+	float* _playerY;
+	LPRECT _clientRect;
 
+	int _displayAreaRight;
+	int _displayAreaBottom;
 
+	LPRECT _mappingRc;
+	LPPOINT _mouseMappingPt;
+
+public:
+	HRESULT init();
+	HRESULT init(int right,int bottom);
+	void setPlayerPosition(float* x,float* y);
+	void release();
+	void startMappingMode(HDC hdc);
+	void endMappingMode(HDC hdc);
 };
 
