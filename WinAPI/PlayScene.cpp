cd924aadf1742bb7cf7213b875bbedb3e5c21c47
plllee96 <plllee96@naver.com>
2022-03-04 10:14:16 +0900

    LHW : Add Boss Clear Effect

--- WinAPI/PlayScene.cpp
+++ WinAPI/PlayScene.cpp
@@ -4,11 +4,10 @@
 HRESULT PlayScene::init(void)
 {
 	_fadeoutImage = IMAGEMANAGER->addImage("Fadeout", "Resource/Images/Lucie/CompleteImg/effect/changeScreen.bmp", 1104, 960, true, RGB(255, 0, 255));
-	_clearBossImage = IMAGEMANAGER->addFrameImage("BossClear", "Resource/Images/Lucie/CompleteImg/effect/clearBoss1.bmp", 5520, 19200, 5, 20, true, RGB(255,0,255));
 	_gameoverImage = IMAGEMANAGER->addImage("Gameover", "Resource/Images/Lucie/CompleteImg/pitures/banana256.bmp", 960, 540, true, RGB(255, 0, 255));
-	// 주석 제거 금지
-	//_clearBossImage2 = IMAGEMANAGER->addFrameImage("BossClear2", "Resource/Images/Lucie/CompleteImg/effect/clearBoss2.bmp", 5520, 19200, 5, 20, true, RGB(255,0,255));
-	//_clearBossImage3 = IMAGEMANAGER->addImage("whiteScreen", "Resource/Images/Lucie/CompleteImg/effect/whiteScreen.bmp", 1104, 960, false, RGB(255, 0, 255));
+	_clearBossImage = IMAGEMANAGER->addFrameImage("BossClear", "Resource/Images/Lucie/CompleteImg/effect/clearBoss1.bmp", 5520, 19200, 5, 20, true, RGB(255,0,255));
+	_clearBossImage2 = IMAGEMANAGER->addFrameImage("BossClear2", "Resource/Images/Lucie/CompleteImg/effect/clearBoss2.bmp", 5520, 19200, 5, 20, true, RGB(255,0,255));
+	_clearBossImage3 = IMAGEMANAGER->addImage("whiteScreen", "Resource/Images/Lucie/CompleteImg/effect/whiteScreen.bmp", 1104, 960, false, RGB(255, 0, 255));
 
 	_mapManager = new MapManager;
 	if (_stageNum == 0)	_mapManager->init(13, _stageNum);
@@ -67,22 +66,21 @@ void PlayScene::update(void)
 	changeMap();
 	checkPlayerEscapeWithoutClear();
 
-	// 주석 제거 금지i
-	//if (_showBossEffect)
-	//{
-	//	if (_bossEffectTime + 0.01f < TIMEMANAGER->getWorldTime())
-	//	{
-	//		if (_frameCount > 225)
-	//		{
-	//			_bossClearAlpha -= 4;
-	//		}
-	//		else
-	//		{
-	//			_frameCount++;
-	//			_bossEffectTime = TIMEMANAGER->getWorldTime();
-	//		}
-	//	}
-	//}
+	if (_showBossEffect)
+	{
+		if (_bossEffectTime + 0.01f < TIMEMANAGER->getWorldTime())
+		{
+			if (_frameCount > 225)
+			{
+				_bossClearAlpha -= 4;
+			}
+			else
+			{
+				_frameCount++;
+				_bossEffectTime = TIMEMANAGER->getWorldTime();
+			}
+		}
+	}
 
 	checkDead();
 	CAMERAMANAGER->update();
@@ -110,12 +108,12 @@ void PlayScene::render(void)
 
 	
 	// 주석 제거 금지
-	//if (_showBossEffect)
-	//{
-	//	if(_frameCount < 100) _clearBossImage->frameRender(getMemDC(), 0, 0, (_frameCount % 5), (_frameCount / 5));
-	//	else if (_frameCount < 200) _clearBossImage2->frameRender(getMemDC(), 0, 0, (_frameCount % 5), ((_frameCount-100) / 5));
-	//	else if (_bossClearAlpha > 0) _clearBossImage3->alphaRender(getMemDC(), _bossClearAlpha);
-	//}
+	if (_showBossEffect)
+	{
+		if(_frameCount < 100) _clearBossImage->frameRender(getMemDC(), -100, - 200, (_frameCount % 5), (_frameCount / 5));
+		else if (_frameCount < 200) _clearBossImage2->frameRender(getMemDC(), -100, -200, (_frameCount % 5), ((_frameCount-100) / 5));
+		else if (_bossClearAlpha > 0) _clearBossImage3->alphaRender(getMemDC(), _bossClearAlpha);
+	}
 
 	if (_player->isDead())
 	{
